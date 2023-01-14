35cf4cd27eb6bf8f3c0b08d6849201c115d404e1
murata <murata@kmc.gr.jp>
2023-01-14 13:29:22 +0900

    wsどうさ

--- yunilang/python/yuni.py
+++ yunilang/python/yuni.py
@@ -10,36 +10,25 @@ class YuniProxy:
     def __init__(self, port, hostname):
         self.port = port
         self.hostname = hostname
-        # asyncio.run(self.websocket_loop())
+        self.ws_loop = self.websocket_loop()
 
-    # async def websocket_loop(self):
-    #     pass
-    #     # async with websockets.connect(f"ws://{self.hostname}:{self.port}") as ws:
-    #     #     await ws.send(yuni_converter.to_packet("Hello World"))
-    #     #     await ws.recv()
-    #     #     print(10)
+    async def websocket_loop(self):
+        async with websockets.connect(f"ws://{self.hostname}:{self.port}") as ws:
+            while True:
+                content = yield True
+                in_packet = yuni_converter.to_packet(content)
+                await ws.send(in_packet)
+                out_packet = await ws.recv()
+                yield yuni_converter.call_packet(out_packet)
 
     async def call(self, content):
-        async with websockets.connect(f"ws://{self.hostname}:{self.port}") as ws:
-            in_packet = yuni_converter.to_packet(content)
-            await ws.send(in_packet)
-            out_packet = await ws.recv()
-            return yuni_converter.call_packet(out_packet)
+        ok = await self.ws_loop.__anext__()
+        if not ok:
+            quit("no connection")
+        return await self.ws_loop.asend(content)
 
     def __getattr__(self, name):
-        result = asyncio.run(self.call(name))
-        print(result)
-        return result
-
-# async def hello():
-#     async with websockets.connect("ws://localhost:7200") as websocket:
-#         await websocket.recv()
-#         print(10)
-#     #     packet = yuni_converter.to_packet("Hello World!")
-#     #     await websocket.send(packet)
-#     #     result = await websocket.recv()
-#     #     print(result)
-# asyncio.run(hello())
+        return asyncio.get_event_loop().run_until_complete(self.call(name))
 
 # python
 py = YuniProxy(7200, "127.0.0.1")
