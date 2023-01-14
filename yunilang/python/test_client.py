a50a8ff633c396e1ed56ed651cf41b3c5e1841de
murata <murata@kmc.gr.jp>
2023-01-15 02:39:48 +0900

    all methods

--- yunilang/python/test_client.py
+++ yunilang/python/test_client.py
@@ -3,12 +3,10 @@ import numpy
 np = yuni.py.numpy
 max = yuni.py.prelude.max
 print(dir(np.array([])))
+print(np.array([1,2.3,3]) + np.array([1,2.3,3]))
 print(numpy.array([1,2.3,3]) + numpy.array([1,2.3,3]))
-x = np.array([1,2,3.3])
-y = np.array([1,2,3.3])
-print(x)
-print(y)
-print(x + y)
+# print(numpy.array([1,2.3,3]) + np.array([1,2.3,3]))
+# print(np.array([1,2.3,3]) + numpy.array([1,2.3,3]))
 
 # TODO: websocket のやりとりを一つの実装だけにして、各言語はstdioでやりとりする
 #       websocket 以外の接続も設計する
