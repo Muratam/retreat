d6fe9cd2c8d6fb39b0a7bcadd61a871f681b64a9
murata <murata@kmc.gr.jp>
2023-01-16 04:21:01 +0900

    node-gyp

--- lang/js/sync-socket/socket.cpp
+++ lang/js/sync-socket/socket.cpp
@@ -1,18 +1,79 @@
 #include <node.h>
-#include <v8.h>
 
-using namespace node;
-using namespace v8;
+namespace yuni_socket {
 
-void Method(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  HandleScope scope(isolate);
-  Local<v8::String> result;
-  MaybeLocal<v8::String> str = String::NewFromUtf8(isolate, "Hello World!");
-  str.ToLocal(&result);
-  args.GetReturnValue().Set(result);
+// need
+// new Socket()
+// - connect(hostname: String, port: Number)
+// - send(bytes: Buffer)
+// - recv(size: Number) -> Buffer
+// - accept() -> [socket:Socket, addr:String]
+// close()
+
+void Add(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+
+  // Check the number of arguments passed.
+  if (args.Length() < 2) {
+    // Throw an Error that is passed back to JavaScript
+    isolate->ThrowException(v8::Exception::TypeError(
+        v8::String::NewFromUtf8(isolate, "Wrong number of arguments")
+            .ToLocalChecked()));
+    return;
+  }
+
+  // Check the argument types
+  if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
+    isolate->ThrowException(v8::Exception::TypeError(
+        v8::String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
+    return;
+  }
+
+  // Perform the operation
+  double value =
+      args[0].As<v8::Number>()->Value() + args[1].As<v8::Number>()->Value();
+  v8::Local<v8::Number> num = v8::Number::New(isolate, value);
+
+  // Set the return value (using the passed in
+  // FunctionCallbackInfo<Value>&)
+  args.GetReturnValue().Set(num);
+}
+void CreateObject(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  v8::Isolate* isolate = args.GetIsolate();
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+  v8::Local<v8::Object> obj = v8::Object::New(isolate);
+  obj->Set(context, v8::String::NewFromUtf8(isolate, "msg").ToLocalChecked(),
+           args[0]->ToString(context).ToLocalChecked())
+      .FromJust();
+
+  args.GetReturnValue().Set(obj);
+}
+void MyFunction(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  auto isolate = args.GetIsolate();
+  args.GetReturnValue().Set(
+      v8::String::NewFromUtf8(isolate, "hello world").ToLocalChecked());
+}
+void CreateFunction(const v8::FunctionCallbackInfo<v8::Value>& args) {
+  auto isolate = args.GetIsolate();
+
+  v8::Local<v8::Context> context = isolate->GetCurrentContext();
+  v8::Local<v8::FunctionTemplate> tpl =
+      v8::FunctionTemplate::New(isolate, MyFunction);
+  v8::Local<v8::Function> fn = tpl->GetFunction(context).ToLocalChecked();
+
+  // omit this to make it anonymous
+  fn->SetName(v8::String::NewFromUtf8(isolate, "theFunction").ToLocalChecked());
+
+  args.GetReturnValue().Set(fn);
 }
 
-void init(Local<Object> exports) { NODE_SET_METHOD(exports, "hello", Method); }
+void Init(v8::Local<v8::Object> exports) {
+  NODE_SET_METHOD(exports, "add", Add);
+  NODE_SET_METHOD(exports, "create", CreateObject);
+  NODE_SET_METHOD(exports, "func", CreateFunction);
+}
 
-NODE_MODULE(NODE_GYP_MODULE_NAME, init)
+NODE_MODULE(NODE_GYP_MODULE_NAME, Init)
+
+}  // namespace yuni_socket
--- lang/js/sync-socket/socket_test.js
+++ lang/js/sync-socket/socket_test.js
@@ -1,3 +1,6 @@
 let socket = require('./build/Release/socket');
-
-console.log(socket.hello());
+console.log(socket.add(3, 5));
+console.log(socket.create("aa"))
+console.log(socket.func)
+console.log(socket.func())
+console.log(socket.func()())
