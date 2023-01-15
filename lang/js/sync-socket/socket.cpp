f807bb026e3e8ef39397ff37746db21aab72e8dd
murata <murata@kmc.gr.jp>
2023-01-16 04:38:06 +0900

    node-gyp

--- lang/js/sync-socket/socket.cpp
+++ lang/js/sync-socket/socket.cpp
@@ -10,61 +10,52 @@ namespace yuni_socket {
 // - accept() -> [socket:Socket, addr:String]
 // close()
 
+v8::Local<v8::String> CreateV8String(v8::Isolate* isolate, const char* str) {
+  return v8::String::NewFromUtf8(isolate, str).ToLocalChecked();
+}
+void ThrowException(v8::Isolate* isolate, const char* str) {
+  auto error_str = CreateV8String(isolate, str);
+  auto error = v8::Exception::TypeError(error_str);
+  isolate->ThrowException(error);
+}
+
 void Add(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  v8::Isolate* isolate = args.GetIsolate();
-
-  // Check the number of arguments passed.
+  auto isolate = args.GetIsolate();
   if (args.Length() < 2) {
-    // Throw an Error that is passed back to JavaScript
-    isolate->ThrowException(v8::Exception::TypeError(
-        v8::String::NewFromUtf8(isolate, "Wrong number of arguments")
-            .ToLocalChecked()));
+    ThrowException(isolate, "Wrong number of arguments");
     return;
   }
-
-  // Check the argument types
   if (!args[0]->IsNumber() || !args[1]->IsNumber()) {
-    isolate->ThrowException(v8::Exception::TypeError(
-        v8::String::NewFromUtf8(isolate, "Wrong arguments").ToLocalChecked()));
+    ThrowException(isolate, "Wrong arguments");
     return;
   }
-
-  // Perform the operation
   double value =
       args[0].As<v8::Number>()->Value() + args[1].As<v8::Number>()->Value();
-  v8::Local<v8::Number> num = v8::Number::New(isolate, value);
-
-  // Set the return value (using the passed in
-  // FunctionCallbackInfo<Value>&)
+  auto num = v8::Number::New(isolate, value);
   args.GetReturnValue().Set(num);
 }
+
 void CreateObject(const v8::FunctionCallbackInfo<v8::Value>& args) {
-  v8::Isolate* isolate = args.GetIsolate();
-  v8::Local<v8::Context> context = isolate->GetCurrentContext();
-
-  v8::Local<v8::Object> obj = v8::Object::New(isolate);
-  obj->Set(context, v8::String::NewFromUtf8(isolate, "msg").ToLocalChecked(),
-           args[0]->ToString(context).ToLocalChecked())
-      .FromJust();
-
+  auto isolate = args.GetIsolate();
+  auto context = isolate->GetCurrentContext();
+  auto obj = v8::Object::New(isolate);
+  auto msg = CreateV8String(isolate, "msg");
+  auto arg0 = args[0]->ToString(context).ToLocalChecked();
+  obj->Set(context, msg, arg0).FromJust();
   args.GetReturnValue().Set(obj);
 }
+
 void MyFunction(const v8::FunctionCallbackInfo<v8::Value>& args) {
   auto isolate = args.GetIsolate();
-  args.GetReturnValue().Set(
-      v8::String::NewFromUtf8(isolate, "hello world").ToLocalChecked());
+  auto str = CreateV8String(isolate, "hello world");
+  args.GetReturnValue().Set(str);
 }
 void CreateFunction(const v8::FunctionCallbackInfo<v8::Value>& args) {
   auto isolate = args.GetIsolate();
-
-  v8::Local<v8::Context> context = isolate->GetCurrentContext();
-  v8::Local<v8::FunctionTemplate> tpl =
-      v8::FunctionTemplate::New(isolate, MyFunction);
-  v8::Local<v8::Function> fn = tpl->GetFunction(context).ToLocalChecked();
-
-  // omit this to make it anonymous
-  fn->SetName(v8::String::NewFromUtf8(isolate, "theFunction").ToLocalChecked());
-
+  auto context = isolate->GetCurrentContext();
+  auto tpl = v8::FunctionTemplate::New(isolate, MyFunction);
+  auto fn = tpl->GetFunction(context).ToLocalChecked();
+  fn->SetName(CreateV8String(isolate, "theFunction"));
   args.GetReturnValue().Set(fn);
 }
 
